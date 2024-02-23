cbf1b5382501ee1c16ac55fe6ac7ac4de89eadd8
LuAndre49 <andre_giancarlo_lu@dlsu.edu.ph>
2024-02-24 00:00:20 +0800

    Added variables to DealersModel, Added DealersController, Added LoginModel

--- Models/DealersModel.cs
+++ Models/DealersModel.cs
@@ -8,10 +8,46 @@ namespace WebApplication1.Models
         [Key]
         public int Id { get; set;}
         [AllowNull]
-        public string? DealerName { get; set;}
+        public string? DealerBusinessName { get; set;}
         [AllowNull]
         public string? DealerAddress { get; set;}
         [DisallowNull]
         public int DealerAccountId { get; set;}
+
+        [AllowNull]
+        public int? DealerTelNo { get; set; }
+        [AllowNull]
+        public int? DealerCellNo { get; set; }
+        [AllowNull]
+        public int? DealerFaxNo { get; set; }
+        [AllowNull]
+        public string? DealerEmail { get; set; }
+        [AllowNull]
+        public string? DealerWebsite { get; set; }
+        [AllowNull]
+        public string? DealerBusinessType { get; set; }
+        [AllowNull]
+        public int? DealerSecNo { get; set; }
+        [AllowNull]
+        public string? DealerDateIssued { get; set; }
+        [AllowNull]
+        public int? DealerAuthorizationCapital { get; set; }
+        [AllowNull]
+        public int? DealerSubscribedCapital { get; set; }
+        [AllowNull]
+        public int? DealerPaidUpCapital { get; set; }
+        [AllowNull]
+        public int? DTIRegNo { get; set; }
+        [AllowNull]
+        public string? DTIDateIssued { get; set; }
+        [AllowNull]
+        public int? DTIAmtCapital { get; set; }
+        [AllowNull]
+        public int? DTIPaidUpCapital { get; set; }
+        [AllowNull]
+        public int? DTITaxAcctNo { get; set; }
+        [AllowNull]
+        public string? DealerTerms { get; set; }
+
     }
 }
