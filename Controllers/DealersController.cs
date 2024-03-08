ced4ad48319820753298ff4a0a98152e8dc2e3a2
adrielteng@gmail.com <adrielteng@gmail.com>
2024-03-08 19:41:17 +0800

    linked view and controllers and fixed form of dealers

--- Controllers/DealersController.cs
+++ Controllers/DealersController.cs
@@ -17,63 +17,69 @@ namespace WebApplication1.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
+            //sql to get top id if no id was sent
+            if(id == 0)
+            {
+
+            }
+
             List<DealersModel> dealersModels = new List<DealersModel>();
             Console.WriteLine("\nQuery data example:");
             Console.WriteLine("=========================================\n");
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["defaultConnection"]))
-                {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
+            //try
+            //{
+            //    using (SqlConnection connection = new SqlConnection(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["defaultConnection"]))
+            //    {
+            //        Console.WriteLine("\nQuery data example:");
+            //        Console.WriteLine("=========================================\n");
 
-                    connection.Open();
+            //        connection.Open();
 
-                    String sql = "SELECT * FROM Dealers";
+            //        String sql = "SELECT * FROM Dealers";
 
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                DealersModel dm = new DealersModel();
-                                dm.Id = reader.GetInt32(0);
-                                dm.DealerBusinessName = reader.GetString(1);
-                                dm.DealerAddress = reader.GetString(2);
-                                dm.DealerTelNo = reader.GetInt32(3);
-                                dm.DealerCellNo = reader.GetInt32(4);
-                                dm.DealerFaxNo = reader.GetInt32(5);
-                                dm.DealerEmail = reader.GetString(6);
-                                dm.DealerWebsite = reader.GetString(7);
-                                dm.DealerBusinessType = reader.GetString(8);
-                                dm.DealerSecNo = reader.GetInt32(9);
-                                dm.DealerDateIssued = reader.GetSqlDateTime(10).Value;
-                                dm.DealerAuthorizationCapital = reader.GetInt32(11);
-                                dm.DealerSubscribedCapital = reader.GetInt32(12);
-                                dm.DealerPaidUpCapital = reader.GetInt32(13);
-                                dm.DTIRegNo = reader.GetInt32(14);
-                                dm.DTIDateIssued = reader.GetSqlDateTime(15).Value;
-                                dm.DTIAmtCapital = reader.GetInt32(16);
-                                dm.DTIPaidUpCapital = reader.GetInt32(17);
-                                dm.DTITaxAcctNo = reader.GetInt32(18);
-                                dm.DealerTerms = reader.GetString(19);
-                                dealersModels.Add(dm);
+            //        using (SqlCommand command = new SqlCommand(sql, connection))
+            //        {
+            //            using (SqlDataReader reader = command.ExecuteReader())
+            //            {
+            //                while (reader.Read())
+            //                {
+            //                    DealersModel dm = new DealersModel();
+            //                    dm.Id = reader.GetInt32(0);
+            //                    dm.DealerBusinessName = reader.GetString(1);
+            //                    dm.DealerAddress = reader.GetString(2);
+            //                    dm.DealerTelNo = reader.GetInt32(3);
+            //                    dm.DealerCellNo = reader.GetInt32(4);
+            //                    dm.DealerFaxNo = reader.GetInt32(5);
+            //                    dm.DealerEmail = reader.GetString(6);
+            //                    dm.DealerWebsite = reader.GetString(7);
+            //                    dm.DealerBusinessType = reader.GetString(8);
+            //                    dm.DealerSecNo = reader.GetInt32(9);
+            //                    dm.DealerDateIssued = reader.GetSqlDateTime(10).Value;
+            //                    dm.DealerAuthorizationCapital = reader.GetInt32(11);
+            //                    dm.DealerSubscribedCapital = reader.GetInt32(12);
+            //                    dm.DealerPaidUpCapital = reader.GetInt32(13);
+            //                    dm.DTIRegNo = reader.GetInt32(14);
+            //                    dm.DTIDateIssued = reader.GetSqlDateTime(15).Value;
+            //                    dm.DTIAmtCapital = reader.GetInt32(16);
+            //                    dm.DTIPaidUpCapital = reader.GetInt32(17);
+            //                    dm.DTITaxAcctNo = reader.GetInt32(18);
+            //                    dm.DealerTerms = reader.GetString(19);
+            //                    dealersModels.Add(dm);
 
-                            }
-                        }
-                    }
-                }
-            }
-            catch (SqlException e)
-            {
-                Debug.WriteLine(e.ToString());
-            }
-            return View(dealersModels);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (SqlException e)
+            //{
+            //    Debug.WriteLine(e.ToString());
+            //}
+            return View();
         }
+        
         [HttpGet]
         public IActionResult Edit(int id)
         {
@@ -147,7 +153,7 @@ namespace WebApplication1.Controllers
                         command.Parameters.AddWithValue("@da", Request.Form["DealerAddress"].ToString());
                         command.Parameters.AddWithValue("@dtn", Request.Form["DealerTelNo"].ToString());
                         command.Parameters.AddWithValue("@dcn", Request.Form["DealerCellNo"].ToString());
-                        command.Parameters.AddWithValue("@dfn", Request.Form["DealerFaxNo"].ToString());
+                        //command.Parameters.AddWithValue("@dfn", Request.Form["DealerFaxNo"].ToString());
                         command.Parameters.AddWithValue("@de", Request.Form["DealerEmail"].ToString());
                         command.Parameters.AddWithValue("@dw", Request.Form["DealerWebsite"].ToString());
                         command.Parameters.AddWithValue("@dbt", Request.Form["DealerBusinessType"].ToString());
--- Program.cs
+++ Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Dealers}/{id?}");
+    pattern: "{controller=Dealers}/{action=index}/{id?}");
 
 app.Run();
